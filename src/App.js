17f8edf3e39028f5b0c12a847e9e094e4cb0fe29
Zack Dev <zackdev@Akays-MacBook-Air.local>
2021-12-03 06:25:09 -0600

    worked on header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import Header from "./components/Header"; 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    <h1>Hello from React</h1>
+    <Header />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,27 @@
 
 
-const Header = () => {
+const Header = ({title}) => {
     return (
         <div>
             <header>
-                <h1>Task Manager</h1>
+                <h1 style ={headerStyle}>{title}</h1>
             </header>
             
         </div>
     )
 }
 
+Header.defaultProps = {
+title :"Task Manager",
+};
+const headerStyle = {
+
+  color :"blue",
+  backgroundColor :"yellow",  
+}
+/*
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+     
+};*/
 export default Header
